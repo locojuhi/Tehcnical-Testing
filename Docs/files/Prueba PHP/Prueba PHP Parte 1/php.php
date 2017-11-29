066c926912b2fe422e5f097a12306af8713bf2f1
danny torres <danny.torresxd@gmail.com>
2017-11-28 22:41:21 -0500

    part_1 complete
    1. ¿Qué sucesión estamos usando para seleccionar las piezas correctas?
    
    fibonacci
    
    2. ¿Estamos descartando algún elemento de la sucesión? ? Cuáles : OK.
    
    los valores de los arrays que su indice no este dentro de la secuencia de fibonacci
    
    3. ¿Qué tipo de codificación estamos usando?
    base64
    4. Enumere y explique los errores encontrados.
    1.- en el for de la linea 9 se estan solicitando un numero de caracateres en cual el array no comienza por 1 sino por 0
    2.- donde estaba la linea 10 se estaba llamando a un indice que no existia el cual se cambio por una funcion recursiva de fibonacci para obtener el indice
    3.-  en la linea 11 no se estaba pasando el parametro de indice no se le pasaba el key sino el valor del que contiene esa posicion del arreglo
    4.- faltaba lo que retorna la funcion get_decoding_result
    
    5.- se estaba negando el if que confirmaba que el indice que se consultaba era mayor al tamaño del array de las opciones disponibles
    5. ¿Cuál es la clave?
    
    "Lorem ipsum dolor sit amet, consectetur adipiscing elit."

--- Docs/files/Prueba PHP/Prueba PHP Parte 1/php.php
+++ Docs/files/Prueba PHP/Prueba PHP Parte 1/php.php
@@ -1,5 +1,4 @@
 <?php
-
 include "Pieces.php";
 
 $fibonacci_seeds = array(0, 1);
@@ -7,20 +6,35 @@ $fibonacci_sequence = $fibonacci_seeds;
 
 $chosen_pieces = array();
 
-for ($i = 0; $i < strlen($pieces[0]); $i++) { 
-    $fibonacci_sequence[$i+2] = $fibonacci_sequence[$i] + $fibonacci_sequence[$i+2];
-    $chosen_pieces[] = get_piece_at_index($pieces, $fibonacci_sequence[$i+2]);
+for ($i = 0; $i < (strlen($pieces[0]) - 1); $i++) {
+    $fibonacci_index = fibonacci($i + 2);
+    $chosen_pieces[] = get_piece_at_index($pieces, $fibonacci_index);
 }
 
+
 $encoded_string = implode("=", $chosen_pieces);
+
 echo get_decoding_result($encoded_string);
 
-function get_decoding_result() {
+function get_decoding_result($encoded_string) {
     // TODO: Return decoded string.
+    return base64_decode ($encoded_string);
 }
 
 function get_piece_at_index($pieces, $index) {
-    return !($index > count($pieces)) ? "" : $pieces[$index];
+    return ($index > count($pieces)) ? "" : $pieces[$index];
+   
 }
 
+function fibonacci($n) {
+	if ($n < 0) {
+		return NULL;
+	} elseif ($n === 0) {
+		return 0;
+	} elseif ($n === 1 || $n === 2) {
+		return 1;
+	} else {
+		return fibonacci($n - 1) + fibonacci($n - 2);
+	}	 
+}
 ?>
